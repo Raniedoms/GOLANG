e1ea9c6c67c2f685355ca398c0ffed3f43bd13cb
rdomsnunes <ranie.nunes@engdb.com.br>
2021-02-27 12:03:47 -0300

    maps

--- firstProject/10- Maps/maps.go
+++ firstProject/10- Maps/maps.go
@@ -14,4 +14,31 @@ func main() {
 	fmt.Println("---------------------------------------")
 	fmt.Println(usuario["nome"])
 
+	usuario2 := map[string]map[string]string{
+
+		"nome": {
+			"primeiro": "João",
+			"ultimo":   "Pedro",
+		},
+		"curso": {
+			"nome":   "Engenharia",
+			"campus": "1",
+		},
+	}
+	fmt.Println(usuario2)
+	fmt.Println(usuario2["nome"])
+	fmt.Println("---------------------------------------")
+
+	//quero apagar um map dentro desses
+	delete(usuario2, "nome")
+	fmt.Println(usuario2)
+
+	fmt.Println("---------------------------------------")
+
+	//quero adicionar um map dentro desses
+	usuario2["signo"] = map[string]string{
+		"nome": "Gêmeos",
+	}
+	fmt.Println(usuario2)
+
 }
