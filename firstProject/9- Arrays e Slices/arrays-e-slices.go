472a76217f7c1b01b6c5501f1b7800eebfcd0ab7
rdomsnunes <ranie.nunes@engdb.com.br>
2021-02-27 11:39:18 -0300

    array

--- firstProject/9- Arrays e Slices/arrays-e-slices.go
+++ firstProject/9- Arrays e Slices/arrays-e-slices.go
@@ -37,4 +37,29 @@ func main() {
 	slice = append(slice, 18)
 	fmt.Println(slice)
 
+	//referenciar um array - fatia a partir de um array que já existia
+	slice2 := array3[1:3]
+	fmt.Println(slice2)
+
+	//ele funciona como o Ponteiro, se a posição for alterada ele irá alterar.
+	array3[1] = "Posicao Alterada"
+	fmt.Println(slice2)
+
+	//ARRAYS INTERNOS
+	//função make - 3 parametros () - 1° paramentro - tipo | 2° parametro - tamanho | 3° parametro - capacidade de itens.
+	//função make ela cria um array
+	fmt.Println("---------------------------------------")
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //lenght
+	fmt.Println(cap(slice3)) //capacidade
+
+	fmt.Println("---------------------------------------")
+	//querendo estourar a capacidade - o GO quando ve que seu slice vai estourar o tamanho ele cria um outro array e dobra o tamanho dele
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //lenght
+	fmt.Println(cap(slice3)) //capacidade
+
 }
